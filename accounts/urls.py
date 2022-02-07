2b37b124e7908bd1ad6864158e470127ccd847d3
rlaalsrbaa <rlaalsrbaa@gmail.com>
2022-02-07 20:52:11 +0900

    비밀번호 찾기 작업중

--- accounts/urls.py
+++ accounts/urls.py
@@ -11,8 +11,8 @@ urlpatterns = [
     path('find_username/', views.find_username, name='find_username'),
     path('login/kakao/', views.kakao_login, name="kakao_login"),
     path('login/kakao/callback/', views.kakao_login_callback, name="kakao_login_callback"),
-    path('reset_password/', auth_views.PasswordResetView.as_view(template_name="accounts/reset_password.html"), name='reset_password'),
-    path('reset_password_sent/', auth_views.PasswordResetDoneView.as_view(template_name="accounts/password_reset_sent.html"), name='password_reset_done'),
-    path('reset/<uidb64>/<token>', auth_views.PasswordResetConfirmView.as_view(template_name="accounts/password_reset_form.html"), name='password_reset_confirm'),
-    path('reset_password_complete/', auth_views.PasswordResetCompleteView.as_view(template_name="accounts/password_reset_done.html"), name='password_reset_complete'),
+    path('reset_password/', auth_views.PasswordResetView.as_view(template_name="accounts/password_reset/reset_password.html"), name='reset_password'),
+    path('reset_password_sent/', auth_views.PasswordResetDoneView.as_view(template_name="accounts/password_reset/password_reset_sent.html"), name='password_reset_done'),
+    path('reset/<uidb64>/<token>', auth_views.PasswordResetConfirmView.as_view(template_name="accounts/password_reset/password_reset_form.html"), name='password_reset_confirm'),
+    path('reset_password_complete/', auth_views.PasswordResetCompleteView.as_view(template_name="accounts/password_reset/password_reset_done.html"), name='password_reset_complete'),
 ]
